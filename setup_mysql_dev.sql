7bdd13f96df2e457c97846af7900c66baa64a85f
Adrian <dreamweaver3d@gmail.com>
2023-11-16 07:25:56 +0800

    airbnb3 task 3 commit 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
 -- setup database for airbnb and add new user + grant appropriate privileges
-CREATE DATABASE IF NOT EXISTS hbtn_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 GRANT ALL PRIVILEGES ON hbtn_dev_db.* TO 'hbnb_dev'@'localhost';

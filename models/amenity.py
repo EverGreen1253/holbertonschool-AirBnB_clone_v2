cdb38b87cf4f34690573b68425f83fb57b1d0daa
Adrian <dreamweaver3d@gmail.com>
2023-11-20 13:42:26 +0800

    airbnb3 task 10 commit 9

--- models/amenity.py
+++ models/amenity.py
@@ -6,6 +6,15 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey, Table
 
 
+if getenv('HBNB_TYPE_STORAGE') is None or getenv('HBNB_TYPE_STORAGE') == "db":
+    place_amenity = Table(
+        'place_amenity',
+        Base.metadata,
+        Column('place_id', String(60), ForeignKey('places.id'), nullable=False, primary_key=True),
+        Column('amenity_id', String(60), ForeignKey('amenities.id'), nullable=False, primary_key=True),
+        extend_existing=True
+    )
+
 class Amenity(BaseModel, Base):
     """ Review class to store review information """
     storage_engine = getenv('HBNB_TYPE_STORAGE')
@@ -15,6 +24,6 @@ class Amenity(BaseModel, Base):
     if storage_engine == "db":
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
-        # place_amenities = relationship("Place", secondary=place_amenity)
+        place_amenities = relationship("Place", secondary=place_amenity)
     else:
         name = ""

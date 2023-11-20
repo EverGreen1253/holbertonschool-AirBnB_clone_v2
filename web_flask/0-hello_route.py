21df28f07b2126cc74d6ea87b8a013b8f5244718
Adrian <dreamweaver3d@gmail.com>
2023-11-20 14:55:03 +0800

    airbnb3wf task 012 commit 1

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Nameless Module for Flask """
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ Prints hello hbnb sting """
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Nameless Module for Flask """
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ Prints hello hbnb string """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Prints hbnb string """
+    return "HBNB!"
+
+@app.route("/c/<text>")
+def c_text(text):
+    """ Prints text passed in """
+    return "C {0}".format(text.replace("_", " "))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)

22786299e4e4f334746c5703b9f7e3debce39c17
Adrian <dreamweaver3d@gmail.com>
2023-11-21 11:32:16 +0800

    airbnbwfw task 5 commit 1

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Nameless Module for Flask """
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -35,10 +35,18 @@ def python_text(text="is_cool"):
 def num_text(n):
     """ Prints text passed in """
     is_num_txt = ""
-    # check if numeric
+    # check if integer
     if n.isnumeric():
         is_num_txt = " is a number"
         return "{0}{1}".format(n, is_num_txt)
+    abort(404)
+
+
+@app.route("/number_template/<n>", strict_slashes=False)
+def num_templ(n):
+    """ Prints template if number passed in """
+    if n.isnumeric():
+        return render_template('5-number.html', num=n)
     abort(404)
 
 if __name__ == "__main__":

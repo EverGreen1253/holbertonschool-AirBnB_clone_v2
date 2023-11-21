bdc9ef862bb7e2c5c020ee7eda25d438bfbc2452
Adrian <dreamweaver3d@gmail.com>
2023-11-21 14:33:00 +0800

    airbnb3wfw Task 7 commit 10

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -112,4 +112,8 @@ class DBStorage:
     def close(self):
         """Close current session"""
         self.__session.remove()
-        self.reload()
+
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()

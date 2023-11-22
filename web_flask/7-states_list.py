09f47b41120f7de0b52a00342641fb18c7ea7a55
Adrian <dreamweaver3d@gmail.com>
2023-11-23 07:44:13 +0800

    airbnb3wfw task 8 commit 1

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Nameless Module for Flask """
+from flask import Flask, render_template
+from models.__init__ import storage
+from models.state import State
+
+app = Flask(__name__)
+
+@app.route("/states_list", strict_slashes=False)
+def states():
+    """ Print out HTML lisitng out all States """
+    states_list = storage.all(State)
+
+    # for key, value in states_list.items():
+    #     print(key)
+    #     print(value['name'])
+    #     print("---")
+
+    return render_template('7-states_list.html', states_list=states_list)
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)

2085354626c46e74f4bda78cfcc90e0d9cd508e7
Adrian <dreamweaver3d@gmail.com>
2023-11-27 14:57:36 +0800

    airbnb3 mysql task 1 commit 3

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -60,7 +60,7 @@ class test_basemodel(unittest.TestCase):
         """ """
         i = self.value()
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
-                         i.__dict__))
+                         i.to_dict()))
 
     def test_todict(self):
         """ """
@@ -77,8 +77,9 @@ class test_basemodel(unittest.TestCase):
     def test_kwargs_one(self):
         """ """
         n = {'Name': 'test'}
-        with self.assertRaises(KeyError):
-            new = self.value(**n)
+        new = self.value(**n)
+
+        self.assertEqual(new.Name, 'test')
 
     def test_id(self):
         """ """

ce01af464c13bd207367f66b5f0d93c5fba7242e
Adrian <dreamweaver3d@gmail.com>
2023-11-27 15:06:33 +0800

    airbnb3 mysql task 1 commit 4

--- models/base_model.py
+++ models/base_model.py
@@ -34,19 +34,22 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+
+        now = datetime.now()
+
         if len(kwargs) > 0:
             if 'id' not in kwargs:
                 kwargs['id'] = str(uuid.uuid4())
 
             if 'updated_at' not in kwargs:
-                kwargs['updated_at'] = datetime.now()
+                kwargs['updated_at'] = now
             else:
                 kwargs['updated_at'] = datetime.strptime(
                     kwargs['updated_at'],
                     '%Y-%m-%dT%H:%M:%S.%f')
 
             if 'created_at' not in kwargs:
-                kwargs['created_at'] = datetime.now()
+                kwargs['created_at'] = now
             else:
                 kwargs['created_at'] = datetime.strptime(
                     kwargs['created_at'],
@@ -58,8 +61,8 @@ class BaseModel:
             self.__dict__.update(kwargs)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = now
+            self.updated_at = now
 
     def __str__(self):
         """Returns a string representation of the instance"""
